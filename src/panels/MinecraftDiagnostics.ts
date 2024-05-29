0d3a82017d8e154a2f010f1f55c76996ff3180e4
Jake Shirley <jake@xbox.com>
2024-05-29 09:12:09 -0700

    Removed unused listener

--- src/panels/MinecraftDiagnostics.ts
+++ src/panels/MinecraftDiagnostics.ts
@@ -25,7 +25,7 @@ export class MinecraftDiagnosticsPanel {
         this._panel.webview.html = this._getWebviewContent(this._panel.webview, extensionUri);
 
         // Set an event listener to listen for messages passed from the webview context
-        this._setWebviewMessageListener(this._panel.webview);
+        //this._setWebviewMessageListener(this._panel.webview);
 
         this._statsCallback = (stat: StatData) => {
             if (stat.parent_id !== undefined) {
@@ -141,32 +141,5 @@ export class MinecraftDiagnosticsPanel {
         </body>
       </html>
     `;
-    }
-
-    /**
-     * Sets up an event listener to listen for messages passed from the webview context and
-     * executes code based on the message that is recieved.
-     *
-     * @param webview A reference to the extension webview
-     * @param context A reference to the extension context
-     */
-    private _setWebviewMessageListener(webview: Webview) {
-        webview.onDidReceiveMessage(
-            (message: any) => {
-                const command = message.command;
-                const text = message.text;
-
-                switch (command) {
-                    case 'hello':
-                        // Code that should run in response to the hello message command
-                        window.showInformationMessage(text);
-                        return;
-                    // Add more switch case statements here as more webview message commands
-                    // are created within the webview context (i.e. inside media/main.js)
-                }
-            },
-            undefined,
-            this._disposables
-        );
     }
 }

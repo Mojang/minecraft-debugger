bbc8b09ca90b9324dbe413fa043f91c266fa0c35
Jake Shirley <jake@xbox.com>
2024-06-20 14:40:17 -0700

    Updated CSP to allow in-line styling for diagnostics pane that uses Observable Plot

--- src/panels/MinecraftDiagnostics.ts
+++ src/panels/MinecraftDiagnostics.ts
@@ -131,7 +131,7 @@ export class MinecraftDiagnosticsPanel {
         <head>
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}';">
           <link rel="stylesheet" type="text/css" href="${stylesUri}">
           <title>Minecraft Diagnostics</title>
         </head>

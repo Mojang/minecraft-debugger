6664dafddd4ada0c9e25b47591aeaea61c490c72
Jake Shirley <jake@xbox.com>
2024-07-08 08:36:36 -0700

    Fixed entity handle view not refreshing when selecting a different plugin

--- webview-ui/src/diagnostics_panel/controls/ScriptPluginSelectionBox.tsx
+++ webview-ui/src/diagnostics_panel/controls/ScriptPluginSelectionBox.tsx
@@ -18,46 +18,49 @@ export default function ScriptPluginSelectionBox({ onChange }: ScriptPluginSelec
     // state
     const [pluginEntries, setPluginEntries] = useState<PluginEntry[]>([{ id: 'no_plugin_selected', name: 'n/a' }]);
 
-    const _onChange = useCallback((e: Event | React.FormEvent<HTMLElement>): void => {
-        const target = e.target as HTMLSelectElement;
-        onChange(pluginEntries[target.selectedIndex].id);
-    }, []);
-
-    const eventHandler = (e: MessageEvent): void => {
-        // Object containing type prop and value prop
-        const msg: MessageEvent = e;
-
-        switch (msg.data.type) {
-            case 'statistic-updated': {
-                if (msg.data.group !== 'handle_counts') {
-                    return;
-                }
-                // Add it to the list
-                setPluginEntries(prevState => {
-                    // See if the plugin is not in the list
-                    if (
-                        prevState === undefined ||
-                        prevState?.findIndex(x => {
-                            return x.id === msg.data.id;
-                        }) === -1
-                    ) {
-                        return [...(prevState ?? []), { id: msg.data.id, name: msg.data.name }];
-                    }
-                    return prevState;
-                });
-            }
-        }
-    };
+    const _onChange = useCallback(
+        (e: Event | React.FormEvent<HTMLElement>): void => {
+            const target = e.target as HTMLSelectElement;
+            onChange(pluginEntries[target.selectedIndex].id);
+        },
+        [pluginEntries]
+    );
 
     //draws chart
     useEffect(() => {
+        const eventHandler = (e: MessageEvent): void => {
+            // Object containing type prop and value prop
+            const msg: MessageEvent = e;
+
+            switch (msg.data.type) {
+                case 'statistic-updated': {
+                    if (msg.data.group !== 'handle_counts') {
+                        return;
+                    }
+                    // Add it to the list
+                    setPluginEntries(prevState => {
+                        // See if the plugin is not in the list
+                        if (
+                            prevState === undefined ||
+                            prevState?.findIndex(x => {
+                                return x.id === msg.data.id;
+                            }) === -1
+                        ) {
+                            return [...(prevState ?? []), { id: msg.data.id, name: msg.data.name }];
+                        }
+                        return prevState;
+                    });
+                }
+            }
+        };
+
         window.addEventListener('message', eventHandler);
 
         // Remove listener
         return () => {
             window.removeEventListener('message', eventHandler);
         };
-    });
+    }, [pluginEntries]);
 
     return (
         <div className="dropdown-container">

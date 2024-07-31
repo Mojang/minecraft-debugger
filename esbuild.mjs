c067632c822b94f008bbf8b29c181e465be5acf5
Jake Shirley <navi@odinseye.org>
2024-07-31 10:39:15 -0700

    fix(build): Source maps would not load due to missing `mappings.wasm` file (fixes #114) (#115)
    I knew that bundling wasn't as easy as it seemed!

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,9 +1,25 @@
-const esbuild = require('esbuild');
+import * as esbuild from 'esbuild';
+import { copy } from 'esbuild-plugin-copy';
 
 const production = process.argv.includes('--production');
 const watch = process.argv.includes('--watch');
 
 async function main() {
+    // Copy source-map wasm file to dist folder
+    const copySourceMapWasm = copy({
+        // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+        // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+        resolveFrom: 'cwd',
+        assets: {
+            from: ['./node_modules/source-map/lib/*.wasm'],
+            to: ['./dist'],
+        },
+        watch: watch,
+    });
+
+    /**
+     * @type {import('esbuild').BuildOptions}
+     */
     const buildOptions = {
         entryPoints: ['src/extension.ts'],
         bundle: true,
@@ -15,7 +31,7 @@ async function main() {
         outfile: 'dist/extension.js',
         external: ['vscode'],
         logLevel: 'silent',
-        plugins: [esbuildProblemMatcherPlugin],
+        plugins: [esbuildProblemMatcherPlugin, copySourceMapWasm],
     };
 
     if (watch) {

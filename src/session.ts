55305189120d37aa222a584f8e904cc6ed80b9f7
Brandon Chan <brandon.chan@skyboxlabs.com>
2025-09-16 21:53:25 -0700

    Restore code

--- src/session.ts
+++ src/session.ts
@@ -1153,6 +1153,7 @@ export class Session extends DebugSession {
             clearTimeout(timeout);
             timeout = setTimeout(() => {
                 // always clear source maps
+                this._sourceMaps.clearCache();
                 // and optionally reload Minecraft
                 if (reloadOnSourceChangesEnabled) {
                     this.onRunMinecraftCommand('say §aPerforming Auto-Reload');
